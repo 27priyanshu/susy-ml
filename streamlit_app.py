10a99ccfc242d905b7782305a275d1c8b1fe1c04
PRIYANSHU SINGH <95427620+27priyanshu@users.noreply.github.com>
2023-06-14 13:04:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,38 +2,56 @@ import streamlit as st
 import pandas as pd
 import zipfile
 import joblib
+import os
+
+# Define the path to the model file
+model_file_path = os.path.join(os.getcwd(), "logistic_regression_model.pkl")
 
 # Load the logistic regression model
-model = joblib.load('logistic_regression_model.pkl')
+try:
+    model = joblib.load(model_file_path)
+except FileNotFoundError:
+    st.error("Model file not found. Please make sure the model file exists.")
+    st.stop()
+except Exception as e:
+    st.error(f"Error loading the model: {str(e)}")
+    st.stop()
+
+# Define the Streamlit app
 def main():
-    st.title('Anomaly Detection')
-    
-    file = st.file_uploader('Upload CSV file or ZIP file', type=['csv', 'zip'])
-    
-    if file is not None:
-        df = process_file(file)
-        if df is not None:
-            prediction = model.predict(df)
-            st.write('Prediction:', prediction)
+    st.title("Susy ML App")
+    st.write("Welcome to the Susy ML App!")
 
-def process_file(file):
-    if file.type == 'application/zip':
-        try:
-            with zipfile.ZipFile(file, 'r') as zip_ref:
-                csv_filename = zip_ref.namelist()[0]
-                with zip_ref.open(csv_filename) as csv_file:
-                    df = pd.read_csv(csv_file)
-        except:
-            st.error('Invalid zip file')
-            return None
-    else:
-        try:
-            df = pd.read_csv(file)
-        except:
-            st.error('Invalid file format')
-            return None
-    
-    return df
+    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv", "zip"])
 
-if __name__ == '__main__':
+    if uploaded_file is not None:
+        # Check if the file is a zip file
+        if uploaded_file.name.endswith('.zip'):
+            try:
+                with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
+                    # Extract the CSV file from the zip
+                    csv_filename = zip_ref.namelist()[0]
+                    zip_ref.extract(csv_filename)
+                    # Read the CSV file using pandas
+                    df = pd.read_csv(csv_filename)
+            except Exception as e:
+                st.error(f"Error processing the zip file: {str(e)}")
+                st.stop()
+
+        else:
+            # Read the CSV file directly using pandas
+            try:
+                df = pd.read_csv(uploaded_file)
+            except Exception as e:
+                st.error(f"Error reading the CSV file: {str(e)}")
+                st.stop()
+
+        # Perform prediction using the loaded model
+        prediction = model.predict(df)
+
+        # Display the prediction result
+        st.subheader("Prediction Results")
+        st.write(prediction)
+
+if __name__ == "__main__":
     main()
