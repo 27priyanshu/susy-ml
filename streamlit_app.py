8518aff9ff491c547a9bbeb1da63e9951a347da1
PRIYANSHU SINGH <95427620+27priyanshu@users.noreply.github.com>
2023-06-14 12:51:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,41 @@
-from flask import Flask, render_template, request
+import streamlit as st
 import pandas as pd
 import zipfile
 from sklearn.linear_model import LogisticRegression
-
-app = Flask(__name__)
 
 # Load the logistic regression model
 model = LogisticRegression()
 model.load_model('logistic_regression_model.pkl')  # Assuming you have saved the trained model
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Check if a file was uploaded
-    if 'file' not in request.files:
-        return render_template('index.html', error='No file uploaded')
-
-    file = request.files['file']
+def main():
+    st.title('Anomaly Detection')
     
-    # Check if a file was selected
-    if file.filename == '':
-        return render_template('index.html', error='No file selected')
+    file = st.file_uploader('Upload CSV file or ZIP file', type=['csv', 'zip'])
+    
+    if file is not None:
+        df = process_file(file)
+        if df is not None:
+            prediction = model.predict(df)
+            st.write('Prediction:', prediction)
 
-    # Check if the file is a zip file
-    if file.filename.endswith('.zip'):
+def process_file(file):
+    if file.type == 'application/zip':
         try:
             with zipfile.ZipFile(file, 'r') as zip_ref:
-                # Extract the CSV file from the zip
                 csv_filename = zip_ref.namelist()[0]
-                zip_ref.extract(csv_filename)
-                # Read the CSV file using pandas
-                df = pd.read_csv(csv_filename)
+                with zip_ref.open(csv_filename) as csv_file:
+                    df = pd.read_csv(csv_file)
         except:
-            return render_template('index.html', error='Invalid zip file')
-
+            st.error('Invalid zip file')
+            return None
     else:
-        # Read the CSV file directly using pandas
         try:
             df = pd.read_csv(file)
         except:
-            return render_template('index.html', error='Invalid file format')
-
-    # Perform prediction using the loaded model
-    prediction = model.predict(df)
-
-    # Return the prediction result to the user
-    return render_template('index.html', prediction=prediction)
+            st.error('Invalid file format')
+            return None
+    
+    return df
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
